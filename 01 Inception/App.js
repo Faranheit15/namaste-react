3eedd403a074d389ea0577eb1827d03119491997
Faranheit15 <ffaranm15@gmail.com>
2023-10-22 17:19:58 +0530

    E01P04 complete

--- 01 Inception/App.js
+++ 01 Inception/App.js
@@ -8,14 +8,16 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
  * </div>
  */
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
+const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", { key: "h1" }, "I'm an h1 tag"),
     React.createElement("h2", { key: "h2" }, "I'm an h2 tag"),
-  ])
-);
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { key: "h11" }, "I'm an h1 tag"),
+    React.createElement("h2", { key: "h22" }, "I'm an h2 tag"),
+  ]),
+]);
 
 // const heading = React.createElement(
 //   "h1",
