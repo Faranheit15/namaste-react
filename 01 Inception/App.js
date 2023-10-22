c5edc04a0a869d4d234a80cd65034b3eba8f1d63
Faranheit15 <ffaranm15@gmail.com>
2023-10-22 17:16:35 +0530

    creating nested structures inside react

--- 01 Inception/App.js
+++ 01 Inception/App.js
@@ -1,10 +1,28 @@
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" },
-  "Hello, World from React!"
+
+/**
+ * <div id="parent">
+ *  <div id="child">
+ *    <h1><I am h1 tag/h1>
+ *  </div>
+ * </div>
+ */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { key: "h1" }, "I'm an h1 tag"),
+    React.createElement("h2", { key: "h2" }, "I'm an h2 tag"),
+  ])
 );
 
-console.log(heading); //object
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", xyz: "abc" },
+//   "Hello, World from React!"
+// );
 
-root.render(heading);
+//console.log(heading); ////object
+
+root.render(parent);
